3ef6f667c9b17dc63995028229bdcc7f9cfc9983
sawepeter <sawepeter6@gmail.com>
2023-04-05 13:36:47 +0300

    code refactor

--- src/components/NewBookForm.js
+++ src/components/NewBookForm.js
@@ -5,8 +5,24 @@ const NewBookForm = () => {
     const { addBook } = useContext(BookContext);
     const {title, setTitle} = useState('');
     const {author, setAuthor} = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-   
+        addBook(title, author);
+        setAuthor('');
+        setTitle('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type='text' placeholder='book title' value={title}
+            onChange={(e) => setTitle(e.target.value)} required />
+            <input type='text' placeholder='book title' value={title}
+            onChange={(e) => setTitle(e.target.value)} required />
+            <input type='submit' value="add book" />
+        </form>
+
+    );
 }
  
 export default NewBookForm;
